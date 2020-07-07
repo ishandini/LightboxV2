15ee2b98b306c3fc9ef500c9579fe9ea9de97662
ishan <ishan@jrcsoftware.com>
2020-07-07 15:09:58 +0530

    frameworks updated.

---
+++ iOSSample/iOSSample.h
@@ -0,0 +1,19 @@
+//
+//  iOSSample.h
+//  iOSSample
+//
+//  Created by Ishan on 7/7/20.
+//  Copyright © 2020 AI Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSSample.
+FOUNDATION_EXPORT double iOSSampleVersionNumber;
+
+//! Project version string for iOSSample.
+FOUNDATION_EXPORT const unsigned char iOSSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSSample/PublicHeader.h>
+
+
